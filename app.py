5d17e8c548c2e2bac70feb0cf22e705417b9e118
Timothy Ngorima <timothytakudzwa@gmail.com>
2019-12-10 11:32:28 +0200

    Adding new emmiters

--- app.py
+++ app.py
@@ -9,22 +9,26 @@ app.config['DEBUG'] = True
 socketio = SocketIO(app)
 
 
-@app.route('/test', methods = ['POST', 'GET'])
+@app.route('/notify', methods = ['POST', 'GET'])
 def test():
+    print('-----------------------------------------------------')
     if request.method == 'POST':
         data = request.get_json()
-        message = data['message']
-        user = data['user']
-        socketio.emit('newnumber', {'message': message, 'user':user}, namespace='/test')
+        notification = data['notification']
+        user_id = data['user_id']
+        action = data['action']
+        if action == 'new_offer':
+            socketio.emit('newnumber', {'notification': notification, 'user_id':user_id}, namespace='/test')
+        elif action == 'new_request':
+            socketio.emit('new_request', {'notification': notification, 'user_id':user_id}, namespace='/new_request')
+        elif action == 'ofer_accepted':
+            socketio.emit('offer_accepted', {'notification': notification, 'user_id':user_id}, namespace='/offer_accepted')
+        elif action == 'offer_rejected':
+            socketio.emit('offer_rejected', {'notification': notification, 'user_id':user_id}, namespace='/offer_rejected')
+        elif action == 'counter_offer':
+            socketio.emit('counter_offer', {'notification': notification, 'user_id':user_id}, namespace='/counter_offer')
     return render_template('index.html')
 
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    print('Client connected')
-    
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
-    print('Client disconnected')
 
 
 if __name__ == '__main__':
--- static/js/application.js
+++ static/js/application.js
@@ -1,9 +1,9 @@
 //Client-side Javascript code for handling random numbers
 $(document).ready(function(){
     var socket = io.connect('http://' + document.domain + ':' + location.port + '/test');
+    console.log(socket)
     socket.on('newnumber', function(msg) {
-        console.log("message  " + msg.message.toString());
-        console.log("user  " + msg.user.toString());
+       console.log('New items')
         
     });
 });
